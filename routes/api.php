bc940627ddeaa0a45ea124431aaf340509d92028
ngocps35536 <doanvanngoc061220@gmail.com>
2024-10-05 17:32:14 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+use App\Http\Controllers\ProductCatalogController;
+use App\Http\Controllers\ProductController;
+
+Route::apiResource('products', ProductController::class);
+
+Route::get('/product-catalogs', [ProductCatalogController::class, 'index']);
+Route::get('/product-catalogs/{id}', [ProductCatalogController::class, 'show']);
+
+
+
+use App\Http\Controllers\AnimalController;
+
+Route::apiResource('animals', AnimalController::class);
