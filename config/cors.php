f4f2d99e49f197555f516d6b6a384d68ad70d468
phuccuhp211 <85235121+phuccuhp211@users.noreply.github.com>
2024-11-05 11:22:09 +0700

    submit cors

--- config/cors.php
+++ config/cors.php
@@ -16,7 +16,7 @@ return [
     */
 
     'paths' => ['*'],
-    'allowed_methods' => ['GET', 'POST', 'PUT', 'OPTIONS'],
+    'allowed_methods' => ['GET', 'POST'],
     'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['Origin', 'Content-Type', 'X-Auth-Token', 'Cookie'],
